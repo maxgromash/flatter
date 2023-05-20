cfa9951bb68d0389c2b17e21812224f1a6944cd0
mgrebenshchikov <mgrebenshchikov@ozon.ru>
2023-05-20 14:25:54 +0300

    minor fixes 2

--- androidApp/src/main/java/com/app/flatter/android/ui/flats/projectDetails/MetroStepView.kt
+++ androidApp/src/main/java/com/app/flatter/android/ui/flats/projectDetails/MetroStepView.kt
@@ -1,12 +1,11 @@
 package com.app.flatter.android.ui.flats.projectDetails
 
 import android.content.Context
+import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
-import com.app.flatter.android.R
 import com.app.flatter.android.databinding.ViewMetroStepBinding
 import com.app.flatter.businessModels.ProjectModel
 
@@ -15,14 +14,13 @@ class MetroStepView(context: Context, attr: AttributeSet? = null) : ConstraintLa
     private val binding = ViewMetroStepBinding.inflate(LayoutInflater.from(context), this)
 
     fun bind(item: ProjectModel.NearestTransport) {
-        //TODO починить цвет
-        binding.metroColorACIV.background = getMetroBackground(R.color.green)
+        binding.metroColorACIV.background = getMetroBackground(Color.parseColor(item.color))
         binding.titleMTV.text = item.name
         binding.timeMTV.text = item.time.toString() + " мин"
     }
 
     private fun getMetroBackground(colorX: Int): GradientDrawable = GradientDrawable().apply {
         shape = GradientDrawable.OVAL
-        color = ContextCompat.getColorStateList(context, colorX)
+        setColor(colorX)
     }
 }
